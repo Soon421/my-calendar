be67144cf1e8361f21eadb1d404f16977273de2b
SoonHwan <shk01421@naver.com>
2025-11-26 01:11:50 +0900

    스마트폰 배포 가이드 추가 및 앱 아이콘 변경

--- vite.config.js
+++ vite.config.js
@@ -20,14 +20,14 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'favicon.svg',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/svg+xml'
+            type: 'image/png'
           },
           {
-            src: 'favicon.svg',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/svg+xml'
+            type: 'image/png'
           }
         ]
       }
